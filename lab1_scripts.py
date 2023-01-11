3652c1db586a560a5757a0c1291c125774b74eb7
Krizzia Concepcion <krizzia@ualberta.ca>
2023-01-10 23:18:05 -0700

    updated script to print raw URL content

--- lab1_scripts.py
+++ lab1_scripts.py
@@ -1,7 +1,20 @@
+'''
+Resource used:
+Author: Documentation page for "Request" library (no sole author)
+Link: https://requests.readthedocs.io/en/latest/user/quickstart/
+
+Resource was mainly used to find the method that prints content of
+raw url link 
+
+'''
 import requests
 
 #script to print out version of requests library
 #print(requests.__version__)
 
 #script to use GET
-print(requests.get("http://google.com/"))
+#print(requests.get("http://google.com/"))
+
+request = requests.get("https://raw.githubusercontent.com/krizziac/404Lab1/main/lab1_scripts.py?token=GHSAT0AAAAAAB5KYGJF4GFV35ELIL34CKJ4Y56JWAA")
+print(request.text)
+
